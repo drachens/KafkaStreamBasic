86a737833b3fcc18550e690202eefb656ffeaeee
drachen <drachen_665@hotmail.com>
2024-03-08 01:29:30 -0300

    test1

--- Producer/funciones/globales.py
+++ Producer/funciones/globales.py
@@ -1,10 +1,14 @@
 #CANTIDAD DE PRODUCTORES
-DISPOSITIVOS = 3
+def get_dispositivos():
+    return(4)
+_DISPOSITIVOS = 4
 #CANTIDAD DE CONSUMIDORES
-num_consumidores = 3
+num_consumidores = 0
+_CONS = 1
 #Contador de mensajes
-mensajes_maximos = 20
+mensajes_maximos = 50+1
 #PUERTOS LOCALES PARA BROKER1, BROKER2 Y BROKER3
 bootstrap_servers = ['localhost:9094','localhost:9095','localhost:9096']
 #TOPICOS
-TOPICOS = ["sensor_1","sensor_2","sensor_3","sensor_4","sensor_5"]
+_TOPICOS = ["sensor_1","sensor_2","sensor_3","sensor_4","sensor_1"]
+__all__ = ['_DISPOSITIVOS','TOPICOS']
