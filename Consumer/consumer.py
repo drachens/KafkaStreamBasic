aa1554e5c109fd24d614e6f45b7fa8bb308b330a
drachen <drachen_665@hotmail.com>
2024-03-08 03:18:08 -0300

    v2024.1

--- Consumer/consumer.py
+++ Consumer/consumer.py
@@ -1,12 +1,7 @@
 from kafka import KafkaConsumer
 import threading
-import subprocess
-from Producer.funciones.globales import *
-#import Producer.funciones.globales.get_dipositivos as lolaso
-#import ..Producer.funciones.globales
 import random
 import json
-import sys
 import os
 import time
 from dotenv import load_dotenv
@@ -17,26 +12,18 @@ load_dotenv()
 def consume_messages(consumer,consumer_data):
     for message in consumer:
         message_dict = json.loads(message.value)
-        #timestamp = message_dict["timestamp"]
-        #value = message_dict["value"]
         name = message_dict["name"]
         consumer_name = consumer_data["name"]
-        #output = f"Dispositivo {name} enviando: Timestamp: {timestamp}, value: {value}"
         output = f"Dispositivo {name} enviando: {message_dict} del consumidor:{consumer_name}"
         print(output)
-# Crear los consumidores en hilos separados
-
 
 if __name__ == '__main__':
     group_id = 'tarea2_consumer'
     topicos = os.environ['TOPICOS'].split(',')
     num_consumidores = int(os.environ['NUM_CONSUMIDORES'])
     bootstrap_server = os.environ['BOOTSTRAP_SERVER'].split(',')
-    print(bootstrap_server)
-    print(type(bootstrap_server))
     threads = []
     consumers = []
-    #print(f"dispositivos":{TOPICOS})
     for i in range(num_consumidores):
         try:
             topic = random.choice(topicos)
@@ -53,26 +40,16 @@ if __name__ == '__main__':
                 "name": "consumer-"+str(i)
             }
             consumers.append([consumer,consumer_data])
-            print(f"suscription:{consumer.subscription()} consumer_data:{consumer_data}")
-            print(f"\ni:{i}")
         except KeyError as e:
             print(f"Error {consumer}")
-    print("rer")
+
+    # Crear los consumidores en hilos separados
     for consumer in consumers:
         thread = threading.Thread(target=consume_messages, args=(consumer[0],consumer[1]))
         thread.start()
         threads.append(thread)
-
-        #consumer[0].close()
-        consumer_x = consumer[0]
-        topics = consumer_x.topics()
-        print(f"consumer:{consumer_x}\ntopics:{topics}")
     for thread in threads:
         try:
             thread.join(timeout=1)
         except RuntimeError as e:
             print(f"Error:{e}")
-    print("sas")
-    #for consumer in consumers:
-        
-    print("pip")
